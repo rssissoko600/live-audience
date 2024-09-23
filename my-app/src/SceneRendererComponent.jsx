dcf5cef71ff5f12e9436986d564c10cce2a90799
nician20 <nician@umich.edu>
2024-09-23 18:05:28 -0400

    can see the scene

--- my-app/src/SceneRendererComponent.jsx
+++ my-app/src/SceneRendererComponent.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, appendSceneAsync, loadSceneAsync } from "@babylonjs/core";
+import { ArcRotateCamera, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, appendSceneAsync, loadSceneAsync } from "@babylonjs/core";
 import SceneComponent from "./SceneComponent"; // uses above component in same directory
 // import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
+
 import "@babylonjs/loaders/OBJ/objFileLoader";
-import "@babylonjs/loaders/glTF";
 import "@babylonjs/loaders";
 
 let box;
 let rpm;
+let audi;
 
 const onSceneReady = async (scene) => {
   // This creates and positions a free camera (non-mesh)
-  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  const camera = new FreeCamera("camera1", new Vector3(-15, 15, 0), scene);
 
   // This targets the camera to scene origin
   camera.setTarget(Vector3.Zero());
@@ -23,16 +24,21 @@ const onSceneReady = async (scene) => {
   camera.attachControl(canvas, true);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  const light = new HemisphericLight("light", new Vector3(0, 10, 0), scene);
 
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  await appendSceneAsync("Scene/Auditorium.obj", scene);
+  // await appendSceneAsync("Scene/Auditorium.obj", scene).then((result) => {
+  //   console.log(result);
+  // });
 
-  SceneLoader.ImportMeshAsync("", "Scene/", "Auditorium.obj").then((result) => {
+  await SceneLoader.ImportMeshAsync("", "Scene/", "Auditorium.obj").then((result) => {
     console.log(result);
+    result.meshes[0].scaling = new Vector3(1, 1, 1);
+
   });
+
   // SceneLoader.ImportMesh("", "Scene/", "Auditorium.obj");
 
 };
@@ -40,17 +46,20 @@ const onSceneReady = async (scene) => {
 /**
  * Will run on every frame render.  We are spinning the box on y-axis.
  */
-const onRender = (scene) => {
-  if (box !== undefined) {
-    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+const onRender = async (scene) => {
+  // if (box !== undefined) {
+  //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
     
-    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
-  }
+  //   box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  // }
+
+  // console.log(audi.meshes);
+
 };
 
 export default (props) => (
   rpm = props.data,
   <div>
-    <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" />
+    <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" width="750" height="500" />
   </div>
 );
