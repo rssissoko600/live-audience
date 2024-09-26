953644a51e58877efb8111a82486c1c895d06e58
Rokiatou Sissoko <rssissoko600@gmail.com>
2024-09-26 08:17:25 -0400

    removed landing page from app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,7 @@ import './App.css';
 import SceneRendererComponent from './SceneRendererComponent';
 import Dictaphone from './SpeechRecognitionComponent';
 import React from 'react';
-
-
-// Landing Page Component
-const LandingPage = () => (
-  <div className="landing-page">
-    <title>Live Audience | proto</title>
-    <img src="/Images/trans_l_a_logo.png" alt="LA Logo" name="logo"/>
-    <p>Click the button below to start the Speech Recognition Test!</p>
-    <Link to="/test">
-      <button className="start-btn">Join Now</button>
-    </Link>
-  </div>
-);
+import LandingPage from './LandingPageComponent';
 
 class SpeechRecognitionTest extends React.Component {
   constructor(props) {
