e4a80221c841141d670f668ff352c732ebeb72d9
Evan Binder <47007288+evnb@users.noreply.github.com>
2024-09-26 03:28:09 -0400

    Implemented mesh instancing. Major Performance improvements.

--- my-app/src/SceneRendererComponent.jsx
+++ my-app/src/SceneRendererComponent.jsx
@@ -1,14 +1,12 @@
-import React, {useEffect} from "react";
-import { ArcRotateCamera, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, appendSceneAsync, loadSceneAsync } from "@babylonjs/core";
+import React, { useEffect } from "react";
+import { FreeCamera, Vector3, HemisphericLight, SceneLoader } from "@babylonjs/core";
 import SceneComponent from "./SceneComponent"; // uses above component in same directory
-// import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
-
 import "@babylonjs/loaders/OBJ/objFileLoader";
 import "@babylonjs/loaders";
 import * as BABYLON from '@babylonjs/core';
 
-let skeletons = [];
+let skeleton;
 let move;
 let exportscene;
 
@@ -28,76 +26,73 @@ const onSceneReady = async (scene, props) => {
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
   const light = new HemisphericLight("light", new Vector3(0, 10, 0), scene);
-
-  // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
+  // Load the auditorium scene
   await SceneLoader.ImportMeshAsync("", "Scene/", "Auditorium.glb").then((result) => {
     result.meshes[0].scaling = new Vector3(1, 1, 1);
   });
 
-  for(let i = 0; i < 8; i++) {
-    for(let j = 0; j < 7 + i; j++) {
-      await SceneLoader.ImportMeshAsync("", "Characters/", "character2.babylon").then((result) => {
-        var skeleton = result.skeletons[0];
-        skeletons.push(result);
-        var scalingStart = new Vector3(0.01, 0.01, 0.01);
-        var positionStart = new Vector3(7,-3,3);
-        var positionAdd = new Vector3(i, i/2, i/2 -j);
-        var rotationStart = new Vector3(-1.5,1.5,0);
-        result.meshes[0].scaling = scalingStart;
-        result.meshes[0].position = positionStart.add(positionAdd);
-        result.meshes[0].rotation = rotationStart;
+  // Load the character mesh and skeleton once
+  let characterTemplate0, characterTemplate1;
+  await SceneLoader.ImportMeshAsync("", "Characters/", "character2.babylon").then((result) => {
+    characterTemplate0 = result.meshes[0]; // Use the first mesh as a template
+    characterTemplate1 = result.meshes[1]; // Use the first mesh as a template
+    skeleton = result.skeletons[0]; // Use one skeleton for all characters
+
+    // After creating all the instances/clones
+    // characterTemplate.dispose(); // Remove the original mesh from the scene and free memory
+
+    // Initialize characters with independent transforms but shared skeleton
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 7 + i; j++) {
+        // Clone the mesh for each character
+        let clone0 = characterTemplate0.clone(`characterClone0_${i}_${j}`);
+        let clone1 = characterTemplate1.clone(`characterClone1_${i}_${j}`);
         
-        result.meshes[1].scaling = scalingStart;
-        result.meshes[1].position = positionStart.add(positionAdd);
-        result.meshes[1].rotation = rotationStart;
+        // Set scaling, position, and rotation for each clone
+        let scalingStart = new Vector3(0.01, 0.01, 0.01);
+        let positionStart = new Vector3(7, -3, 3);
+        let positionAdd = new Vector3(i, i / 2, i / 2 - j);
+        
+        clone0.scaling = scalingStart;
+        clone0.position = positionStart.add(positionAdd);
+        clone0.rotation = new Vector3(-1.5, 1.5, 0);
 
-        var idleRange = skeleton.getAnimationRange(move);
-        // console.log("idle range:", idleRange)
-        // console.log("movement:", move)
+        clone1.scaling = scalingStart;
+        clone1.position = positionStart.add(positionAdd);
+        clone1.rotation = new Vector3(-1.5, 1.5, 0);
 
-        skeleton.animationPropertiesOverride = new BABYLON.AnimationPropertiesOverride();
-        skeleton.animationPropertiesOverride.enableBlending = true;
-        skeleton.animationPropertiesOverride.blendingSpeed = 0.05;
-        skeleton.animationPropertiesOverride.loopMode = 1;
-
-        scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-        });
+        // Bind the clone to the shared skeleton
+        clone0.skeleton = skeleton;
+        clone1.skeleton = skeleton;
+      }
     }
-  }
+
+    characterTemplate0.dispose();
+    characterTemplate1.dispose();
+
+    // Start animation on the shared skeleton
+    const idleRange = skeleton.getAnimationRange(move);
+    scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
+  });
 };
 
 /**
- * Will run on every frame render.  We are spinning the box on y-axis.
+ * Will run on every frame render.
  */
 const onRender = async (scene) => {
-  // if (box !== undefined) {
-  //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-    
-  //   box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
-  // }
-
-  if(skeletons !== undefined) {
-    // console.log(skeletons)
-    // skeletons.forEach((skeleton) => {
-    //   var idleRange = skeleton.getAnimationRange(move);
-    //   scene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
-    // });
-  }
+  // No need to animate multiple skeletons since we are using one shared skeleton
 };
 
 export default (props) => (
   move = props.data,
   exportscene = props.scene,
-  useEffect(() => { 
-    if(skeletons !== undefined) {
-      skeletons.forEach(skel => {
-        var skeleton = skel.skeletons[0];
-        var idleRange = skeleton.getAnimationRange(move);
-         // good has changed the newmove to cheering but scene loader has yet to change animation
-        exportscene.beginAnimation(skeleton, idleRange.from, idleRange.to, true); // skeleton not skel
-      });
+  useEffect(() => {
+    // Trigger animations for the shared skeleton
+    if (skeleton !== undefined) {
+      const idleRange = skeleton.getAnimationRange(move);
+      exportscene.beginAnimation(skeleton, idleRange.from, idleRange.to, true);
     }
   }, [move, exportscene]),
   <div>
