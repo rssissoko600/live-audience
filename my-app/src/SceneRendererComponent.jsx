89fc61b6dd79ff9f8e531c57b40adcbf3ef28085
Evan Binder <47007288+evnb@users.noreply.github.com>
2024-09-26 05:32:48 -0400

    Added left and right audience members. TODO: fix rotation of these characters

--- my-app/src/SceneRendererComponent.jsx
+++ my-app/src/SceneRendererComponent.jsx
@@ -47,25 +47,66 @@ const onSceneReady = async (scene, props) => {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 7 + i; j++) {
         // Clone the mesh for each character
-        let clone0 = characterTemplate0.clone(`characterClone0_${i}_${j}`);
-        let clone1 = characterTemplate1.clone(`characterClone1_${i}_${j}`);
+        let clone0_center = characterTemplate0.clone(`characterClone0_center_${i}_${j}`);
+        let clone1_center = characterTemplate1.clone(`characterClone1_center_${i}_${j}`);
+        let clone0_left = characterTemplate0.clone(`characterClone0_left_${i}_${j}`);
+        let clone1_left = characterTemplate1.clone(`characterClone1_left_${i}_${j}`);
+        let clone0_right = characterTemplate0.clone(`characterClone0_right_${i}_${j}`);
+        let clone1_right = characterTemplate1.clone(`characterClone1_right_${i}_${j}`);
         
         // Set scaling, position, and rotation for each clone
         let scalingStart = new Vector3(0.01, 0.01, 0.01);
         let positionStart = new Vector3(7, -3, 3);
         let positionAdd = new Vector3(i, i / 2, i / 2 - j);
         
-        clone0.scaling = scalingStart;
-        clone0.position = positionStart.add(positionAdd);
-        clone0.rotation = new Vector3(-1.5, 1.5, 0);
+        // let positionAdd_left = new Vector3(
+        //   positionAdd._x * Math.sqrt(3)/2 - positionAdd._y * 0.5, 
+        //   positionAdd._x * 0.5 + positionAdd._y * Math.sqrt(3)/2,
+        //   positionAdd._z);
+        let positionAdd_left = new Vector3(
+          positionAdd._x * Math.sqrt(3)/2 - positionAdd._z * 0.5, 
+          positionAdd._y,
+          positionAdd._x * 0.5 + positionAdd._z * Math.sqrt(3)/2,
+          // positionAdd._y
+        );
+        let positionAdd_right = new Vector3(
+          positionAdd._x * Math.sqrt(3)/2 + positionAdd._z * 0.5, 
+          positionAdd._y,
+          - positionAdd._x * 0.5 + positionAdd._z * Math.sqrt(3)/2,
+          // positionAdd._y
+        );
+        let leftPosOffset = new Vector3(-2.0869- -6.6513, 0, -14.679- -3.2665);
+        let rightPosOffset = new Vector3(-5.1534- -6.6513, 0, 9.0213- -3.2665);
 
-        clone1.scaling = scalingStart;
-        clone1.position = positionStart.add(positionAdd);
-        clone1.rotation = new Vector3(-1.5, 1.5, 0);
+        clone0_center.scaling = scalingStart;
+        clone0_center.position = positionStart.add(positionAdd);
+        clone0_center.rotation = new Vector3(-1.5, 1.5, 0);
+
+        clone1_center.scaling = scalingStart;
+        clone1_center.position = positionStart.add(positionAdd);
+        clone1_center.rotation = new Vector3(-1.5, 1.5, 0);
+
+        clone0_left.scaling = scalingStart;
+        clone0_left.position = positionStart.add(positionAdd_left).subtract(leftPosOffset);
+        clone0_left.rotation = new Vector3(-1.5, 1.5, 0);
+
+        clone1_left.scaling = scalingStart;
+        clone1_left.position = positionStart.add(positionAdd_left).subtract(leftPosOffset);
+        clone1_left.rotation = new Vector3(-1.5, 1.5, 0);
+
+        clone0_right.scaling = scalingStart;
+        clone0_right.position = positionStart.add(positionAdd_right).subtract(rightPosOffset);
+        clone0_right.rotation = new Vector3(-1.5, 0, 0);
+
+        clone1_right.scaling = scalingStart;
+        clone1_right.position = positionStart.add(positionAdd_right).subtract(rightPosOffset);
+        clone1_right.rotation = new Vector3(-1.5, 0, 0);
 
         // Bind the clone to the shared skeleton
-        clone0.skeleton = skeleton;
-        clone1.skeleton = skeleton;
+        clone0_center.skeleton = skeleton;
+        clone1_center.skeleton = skeleton;
+        clone0_left.skeleton = skeleton;
+        clone1_left.skeleton = skeleton;
       }
     }
 
